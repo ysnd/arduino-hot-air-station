56c0bb751c2a07ec236005ece1b8eda70d5570c6
ysnd <yusnadi.aunur@gmail.com>
2026-07-29 21:14:46 +0700

    add adc to temp function using 3 segment interpolate and temp to adc function

--- archive/temperature-read/src/main.cpp
+++ archive/temperature-read/src/main.cpp
@@ -1,14 +1,67 @@
+#include "HardwareSerial.h"
 #include <Arduino.h>
 
-#define THERMOCOUPLE_PIN A0 
+#define THERMOCOUPLE_PIN A0
+
+const uint16_t adc_ambient = 9;
+const uint16_t adc_200 = 587;
+const uint16_t adc_300 = 751;
+const uint16_t adc_400 = 850;
+const uint16_t temp_ambient = 25;
+const uint16_t temp_200 = 200;
+const uint16_t temp_300 = 300;
+const uint16_t temp_400 = 400;
+
+uint16_t clamp_u16(uint16_t val, uint16_t min, uint16_t max) {
+    if (val < min) {
+        return min;
+    }
+    if (val > max) {
+        return max;
+    }
+    return val;
+}
+
+int16_t interpolate(int16_t x, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max) {
+    return out_min + ((x - in_min) * (out_max - out_min)) / (in_max - in_min);
+}
+
+uint16_t adc_to_temp(uint16_t adc) {
+    uint16_t temp;
+    if (adc <= adc_ambient) {
+        temp = temp_ambient;
+    } else if (adc < adc_200) {
+        temp = interpolate(adc, adc_ambient, adc_200, temp_ambient, temp_200);
+    } else if (adc < adc_300) {
+        temp = interpolate(adc, adc_200, adc_300, temp_200, temp_300);
+    } else {
+        temp = interpolate(adc, adc_300, adc_400, temp_300, temp_400);
+    }
+    return temp;
+}
+
+uint16_t temp_to_adc(uint16_t temp) {
+    temp = clamp_u16(temp, 150, 500);
+    uint16_t adc;
+
+    if (temp >= temp_300){
+        adc = interpolate(temp + 1, temp_300, temp_400, adc_300, adc_400);
+    } else {
+        adc = interpolate(temp + 1, temp_200, temp_300, adc_200, adc_300);
+    } 
+    for (uint8_t i=0 ; i<10 ; i++) {
+        if (adc_to_temp(adc) <= temp)
+            break;
+
+        adc--;
+    }
+    return adc;
+}
 
 void setup() {
     Serial.begin(115200); 
 }
 
 void loop() {
-    uint16_t adcVal = analogRead(THERMOCOUPLE_PIN);
-    Serial.print("ADC VAL:" );
-    Serial.println(adcVal);
     delay(200);
 }
