8ba64b1b1f59f9149f3840c123752f0917503357
ysnd <yusnadi.aunur@gmail.com>
2026-08-27 13:12:56 +0700

    refactor encoder read val and delete prev btn read

--- archive/rotary-encoder/src/main.cpp
+++ archive/rotary-encoder/src/main.cpp
@@ -10,18 +10,13 @@
 typedef struct {
     int32_t min_pos;
     int32_t max_pos;
-
     uint8_t a_pin;
     uint8_t b_pin;
-
     bool loop_en;
-
     uint8_t increment;
     uint8_t fast_increment;
-
     uint32_t a_low_start_time;
     uint32_t last_change_time;
-
     bool ch_b_state;
     volatile int16_t pos;
 } encoder_t;
@@ -33,16 +28,13 @@ void encoder_init(encoder_t *enc, uint8_t a_pin, uint8_t b_pin, int16_t init_pos
     enc->a_pin = a_pin;
     enc->b_pin = b_pin;
     enc->pos = init_pos;
-
     enc->min_pos = -32767;
     enc->max_pos = 32766;
     enc->ch_b_state = false;
     enc->increment = 1;
     enc->fast_increment = 2;
-
     enc->last_change_time = 0;
     enc->loop_en = false;
-
     pinMode(enc->a_pin, INPUT_PULLUP);
     pinMode(enc->b_pin, INPUT_PULLUP);
 }
@@ -52,7 +44,6 @@ int16_t encoder_read(encoder_t *enc) {
     noInterrupts();
     pos = enc->pos;
     interrupts();
-
     return pos;
 }
 
@@ -64,17 +55,14 @@ bool encoder_write(encoder_t *enc, int16_t pos) {
     return false;
 }
 
-void encoder_reset(encoder_t *enc, int16_t init_pos, int16_t low, int16_t upp, uint8_t inc, uint8_t fast_inc, bool looped) {
-    enc->min_pos = low;
-    enc->max_pos = upp;
-
+void encoder_reset(encoder_t *enc, int16_t init_pos, int16_t min_pos, int16_t max_pos, uint8_t inc, uint8_t fast_inc, bool looped) {
+    enc->min_pos = min_pos;
+    enc->max_pos = max_pos;
     if (!encoder_write(enc, init_pos)) {
-        enc->pos = low;
+        enc->pos = min_pos;
     }
-
     enc->increment = inc;
     enc->fast_increment = inc;
-
     if (fast_inc > inc) {
         enc->fast_increment = fast_inc;
     }
@@ -92,16 +80,19 @@ void encoder_change_isr(encoder_t *enc) {
         }
     } else {
         if (enc->a_low_start_time > 0) {
-            uint8_t inc = enc->increment;
-            if ((now - enc->a_low_start_time) < ENC_OVER_PRESS) {
+            uint8_t step = enc->increment;
+            uint32_t duration = now - enc->a_low_start_time;
+
+            if (duration < ENC_OVER_PRESS) {
                 if ((now - enc->last_change_time) < ENC_FAST_TIMEOUT) {
-                    inc = enc->fast_increment;
+                    step = enc->fast_increment;
                 }
                 enc->last_change_time = now;
+
                 if (enc->ch_b_state) {
-                    enc->pos -= inc;
+                    enc->pos -= step;
                 } else {
-                    enc->pos += inc;
+                    enc->pos += step;
                 }
                 if (enc->pos > enc->max_pos) {
                     if (enc->loop_en) {
@@ -131,7 +122,6 @@ void setup() {
     Serial.begin(115200);
     pinMode(ENC_SW, INPUT_PULLUP);
     encoder_init(&encoder, ENC_A, ENC_B, 0);
-    
     attachInterrupt(digitalPinToInterrupt(ENC_A), encoder_irq, CHANGE);
 }
 
@@ -143,12 +133,4 @@ void loop() {
         Serial.println(pos);
         old_pos = pos;
     }
-    if (!digitalRead(ENC_SW)) {
-        delay(50);
-        if (!digitalRead(ENC_SW)) {
-            Serial.println("SW Pressed");
-            while (!digitalRead(ENC_SW)); 
-        }
-    }
-    delay(10);
 }
