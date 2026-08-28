12417428057ed31113c1f9add99450d9259dc9c5
ysnd <yusnadi.aunur@gmail.com>
2026-08-28 17:50:17 +0700

    add function button tick

--- archive/button_read/src/main.cpp
+++ archive/button_read/src/main.cpp
@@ -4,6 +4,7 @@
 #define BTN_LONG_TIME 900
 #define BTN_DEBOUNCE 50
 #define BTN_TICK_TIME 200
+#define BTN_OVER_PRESS 3000
 
 typedef enum {
     BUTTON_NONE,
@@ -34,6 +35,8 @@ button_evt_t button_check(button_t *btn) {
         if (!btn->pressed) {
             btn->pressed = true;
             btn->press_time = now;
+        } else if ((now - btn->press_time) > BTN_OVER_PRESS) {
+            btn->press_time = now;
         }
         return BUTTON_NONE;
     }
@@ -43,14 +46,30 @@ button_evt_t button_check(button_t *btn) {
     }
     uint32_t dur = now - btn->press_time;
     btn->pressed = false;
+    btn->tick_time = 0;
 
     if (dur < BTN_DEBOUNCE) {
         return BUTTON_NONE;
     }
-    if (dur < BTN_LONG_TIME) {
-        return BUTTON_SHORT;
+    if (dur > BTN_LONG_TIME) {
+        return BUTTON_LONG;
     }
-    return BUTTON_LONG;
+    return BUTTON_SHORT;
+}
+
+bool button_tick(button_t *btn) {
+    uint32_t now = millis();
+    if (!digitalRead(btn->pin) && btn->pressed && (now - btn->press_time > BTN_LONG_TIME)) {
+        if (now - btn->tick_time > BTN_TICK_TIME) {
+            btn->tick_time = now;
+            return true;
+        }
+        return false;
+    }
+    if (!btn->pressed) {
+        btn->tick_time = 0;
+    }
+    return false;
 }
 
 button_t enc_button;
@@ -74,6 +93,9 @@ void loop() {
 
         default: 
             break;
+    }
+    if (button_tick(&enc_button)) {
+        Serial.println("TICK");
     }
 }
 
