113d3384e8ed563cedb99201417e853fbb2ffdc8
ysnd <yusnadi.aunur@gmail.com>
2026-08-31 12:22:14 +0700

    refactor ui screen state handling

---
+++ archive/ui/src/main.cpp
@@ -0,0 +1,433 @@
+#include "HardwareSerial.h"
+#include <Arduino.h>
+
+#define ENC_A 3 
+#define ENC_B 5 
+#define ENC_SW 4 
+#define MAX_FIXED_POWER 70
+#define ENC_FAST_TIMEOUT 300
+#define ENC_OVER_PRESS 1000
+#define BTN_LONG_TIME 900
+#define BTN_DEBOUNCE 50
+#define BTN_TICK_TIME 200
+#define BTN_OVER_PRESS 3000
+
+typedef struct {
+    int32_t min_pos;
+    int32_t max_pos;
+    uint8_t a_pin;
+    uint8_t b_pin;
+    bool loop_en;
+    uint8_t increment;
+    uint8_t fast_increment;
+    uint32_t a_low_start_time;
+    uint32_t last_change_time;
+    bool ch_b_state;
+    volatile int16_t pos;
+} encoder_t;
+
+typedef enum {
+    BUTTON_NONE,
+    BUTTON_SHORT,
+    BUTTON_LONG
+} button_evt_t;
+
+typedef struct {
+    uint8_t pin;
+    uint32_t press_time;
+    uint32_t tick_time;
+    bool pressed;
+} button_t;
+
+typedef enum {
+    SCREEN_MAIN,
+    SCREEN_CONFIG,
+    SCREEN_CALIB,
+    SCREEN_TUNE,
+    SCREEN_WORK,
+    SCREEN_ERROR
+} screen_id_t;
+
+typedef enum {
+    MAIN_MODE_TEMP,
+    MAIN_MODE_FAN
+} main_mode_t;
+
+typedef enum {
+    CONFIG_CALIB,
+    CONFIG_TUNE,
+    CONFIG_SAVE,
+    CONFIG_CANCEL,
+    CONFIG_DEFAULTS
+} config_mode_t;
+
+typedef enum {
+    CALIB_TEMP_MIN,
+    CALIB_TEMP_MID,
+    CALIB_TEMP_MAX
+} calib_point_t;
+
+typedef struct {
+    screen_id_t current;
+    main_mode_t main_mode;
+    config_mode_t config_mode;
+    calib_point_t calib_point;
+    bool calib_tune;
+    bool tune_on;
+    uint8_t tune_power;
+} ui_t;
+
+encoder_t encoder;
+button_t enc_button;
+ui_t ui;
+
+//Encoder
+void encoder_init(encoder_t *enc, uint8_t a_pin, uint8_t b_pin, int16_t init_pos) {
+    enc->a_low_start_time = 0;
+    enc->a_pin = a_pin;
+    enc->b_pin = b_pin;
+    enc->pos = init_pos;
+    enc->min_pos = -32767;
+    enc->max_pos = 32766;
+    enc->ch_b_state = false;
+    enc->increment = 1;
+    enc->fast_increment = 2;
+    enc->last_change_time = 0;
+    enc->loop_en = false;
+    pinMode(enc->a_pin, INPUT_PULLUP);
+    pinMode(enc->b_pin, INPUT_PULLUP);
+}
+
+int16_t encoder_read(encoder_t *enc) {
+    int16_t pos;
+    noInterrupts();
+    pos = enc->pos;
+    interrupts();
+    return pos;
+}
+
+bool encoder_write(encoder_t *enc, int16_t pos) {
+    if (pos >= enc->min_pos && pos <= enc->max_pos) {
+        enc->pos = pos;
+        return true;
+    }
+    return false;
+}
+
+void encoder_reset(encoder_t *enc, int16_t init_pos, int16_t min_pos, int16_t max_pos, uint8_t inc, uint8_t fast_inc, bool looped) {
+    enc->min_pos = min_pos;
+    enc->max_pos = max_pos;
+    if (!encoder_write(enc, init_pos)) {
+        enc->pos = min_pos;
+    }
+    enc->increment = inc;
+    enc->fast_increment = inc;
+    if (fast_inc > inc) {
+        enc->fast_increment = fast_inc;
+    }
+    enc->loop_en = looped;
+}
+
+void encoder_change_isr(encoder_t *enc) {
+    bool a = digitalRead(enc->a_pin);
+    uint32_t now = millis();
+
+    if (!a) {
+        if ((enc->a_low_start_time == 0) || (now-enc->a_low_start_time > ENC_OVER_PRESS)) {
+            enc->a_low_start_time = now;
+            enc->ch_b_state = digitalRead(enc->b_pin);
+        }
+    } else {
+        if (enc->a_low_start_time > 0) {
+            uint8_t step = enc->increment;
+            uint32_t duration = now - enc->a_low_start_time;
+
+            if (duration < ENC_OVER_PRESS) {
+                if ((now - enc->last_change_time) < ENC_FAST_TIMEOUT) {
+                    step = enc->fast_increment;
+                }
+                enc->last_change_time = now;
+
+                if (enc->ch_b_state) {
+                    enc->pos -= step;
+                } else {
+                    enc->pos += step;
+                }
+                if (enc->pos > enc->max_pos) {
+                    if (enc->loop_en) {
+                        enc->pos = enc->min_pos;
+                    } else {
+                        enc->pos = enc->max_pos;
+                    }
+                }
+                if (enc->pos < enc->min_pos) {
+                    if (enc->loop_en) {
+                        enc->pos = enc->max_pos;
+                    } else {
+                        enc->pos = enc->min_pos;
+                    }
+                }
+            }
+            enc->a_low_start_time = 0;
+        }
+    }
+}
+
+void encoder_irq(void) {
+    encoder_change_isr(&encoder);
+}
+
+//Button
+void button_init(button_t *btn, uint8_t pin) {
+    btn->pin = pin;
+    btn->press_time = 0;
+    btn->tick_time = 0;
+    btn->pressed = false;
+    pinMode(btn->pin, INPUT_PULLUP);
+}
+
+button_evt_t button_check(button_t *btn) {
+    uint32_t now = millis();
+
+    if (!digitalRead(btn->pin)) {
+        //tombol diteken
+        if (!btn->pressed) {
+            btn->pressed = true;
+            btn->press_time = now;
+        } else if ((now - btn->press_time) > BTN_OVER_PRESS) {
+            btn->press_time = now;
+        }
+        return BUTTON_NONE;
+    }
+    //tombol tdk diteken
+    if (!btn->pressed) {
+        return BUTTON_NONE;
+    }
+    uint32_t dur = now - btn->press_time;
+    btn->pressed = false;
+    btn->tick_time = 0;
+
+    if (dur < BTN_DEBOUNCE) {
+        return BUTTON_NONE;
+    }
+    if (dur > BTN_LONG_TIME) {
+        return BUTTON_LONG;
+    }
+    return BUTTON_SHORT;
+}
+
+bool button_tick(button_t *btn) {
+    uint32_t now = millis();
+    if (!digitalRead(btn->pin) && btn->pressed && (now - btn->press_time > BTN_LONG_TIME)) {
+        if (now - btn->tick_time > BTN_TICK_TIME) {
+            btn->tick_time = now;
+            return true;
+        }
+        return false;
+    }
+    if (!btn->pressed) {
+        btn->tick_time = 0;
+    }
+    return false;
+}
+
+//UI 
+void ui_init(ui_t *ui) {
+    ui->current = SCREEN_MAIN;
+    ui->main_mode = MAIN_MODE_TEMP;
+    ui->config_mode = CONFIG_CALIB;
+    ui->calib_point = CALIB_TEMP_MIN;
+    ui->calib_tune = false;
+    ui->tune_on = false;
+    ui->tune_power = MAX_FIXED_POWER >> 2;
+}
+
+void ui_set_screen(ui_t *ui, screen_id_t next) {
+    ui->current = next;
+}
+
+screen_id_t ui_get_screen(ui_t *ui) {
+    return ui->current;
+}
+
+void config_rotate(ui_t *ui, int16_t val) {
+    if (val < CONFIG_CALIB) {
+        val = CONFIG_CALIB;
+    }
+    if (val > CONFIG_DEFAULTS) {
+        val = CONFIG_DEFAULTS;
+    }
+    ui->config_mode = (config_mode_t)val;
+    Serial.print("CONFIG selection = ");
+    Serial.println(val);
+}
+
+void config_short_press(ui_t *ui) {
+    switch (ui->config_mode) {
+        case CONFIG_CALIB:
+           ui_set_screen(ui, SCREEN_CALIB);
+            Serial.println("CONFIG -> CALIB");
+            break;
+
+        case CONFIG_TUNE:
+            ui_set_screen(ui, SCREEN_TUNE);
+            Serial.println("CONFIG -> TUNE");
+            break;
+
+        case CONFIG_SAVE:
+            Serial.println("CONFIG: Save selected");
+            break;
+
+        case CONFIG_CANCEL:
+            ui_set_screen(ui, SCREEN_MAIN);
+            Serial.println("CONFIG -> MAIN");
+            break;
+
+        case CONFIG_DEFAULTS:
+            Serial.println("CONFIG: DEFAULTS selected");
+            break;
+    }
+}
+
+void tune_rotate(ui_t *ui, int32_t val) {
+    if (ui->current != SCREEN_TUNE) {
+        return;
+    }
+    if (val < 0) {
+        val = 0;
+    }
+    if (val > MAX_FIXED_POWER) {
+        val = MAX_FIXED_POWER;
+    }
+    ui->tune_power = val;
+    Serial.print("TUNE PWR = ");
+    Serial.println(val);
+}
+
+void tune_short_press(ui_t *ui) {
+    if (ui->tune_on) {
+        ui->tune_on = false;
+        Serial.println("TUNE: OFF");
+    } else {
+        ui->tune_on = true;
+        Serial.print("TUNE: ON, power = ");
+        Serial.println(ui->tune_power);
+    }
+}
+
+void tune_long_press(ui_t *ui) {
+    ui->tune_on = false;
+    Serial.print("TUNE: OFF");
+    Serial.println("TUNE -> MAIN");
+    ui_set_screen(ui, SCREEN_MAIN);
+}
+
+void ui_rotate(ui_t *ui, int16_t val) {
+    switch (ui->current) {
+        case SCREEN_MAIN:
+            if (ui->main_mode == MAIN_MODE_TEMP) {
+                Serial.print("MAIN TEMP = ");
+            } else {
+                Serial.print("MAIN FAN = ");
+            }
+            Serial.println(val);
+            break;
+
+        case SCREEN_CONFIG:
+            config_rotate(ui, val);
+            break;
+
+        case SCREEN_TUNE:
+            tune_rotate(ui, val);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void ui_short_press(ui_t *ui) {
+    switch (ui->current) {
+        case SCREEN_MAIN:
+            if (ui->main_mode == MAIN_MODE_TEMP) {
+                ui->main_mode = MAIN_MODE_FAN;
+                Serial.println("MAIN: TEMP -> FAN");
+            } else {
+                ui->main_mode = MAIN_MODE_TEMP;
+                Serial.println("MAIN: FAN -> TEMP");
+            }
+            break;
+
+        case SCREEN_CONFIG:
+            config_short_press(ui);
+            break;
+
+        case SCREEN_TUNE:
+            tune_short_press(ui);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void ui_long_press(ui_t *ui) {
+    switch (ui->current) {
+        case SCREEN_MAIN:
+            ui_set_screen(ui, SCREEN_CONFIG);
+            Serial.println("MAIN -> CONFIG");
+            break;
+
+        case SCREEN_TUNE:
+            tune_long_press(ui);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    encoder_init(&encoder, ENC_A, ENC_B, 0);
+    button_init(&enc_button, ENC_SW);
+    ui_init(&ui);
+    Serial.print("Current screen ");
+    Serial.println(ui_get_screen(&ui));
+    attachInterrupt(digitalPinToInterrupt(ENC_A), encoder_irq, CHANGE);
+}
+
+void loop() {
+    static int16_t old_pos = 0;
+    int16_t pos = encoder_read(&encoder);
+    static uint8_t ui_pos = 0;
+    if (pos != old_pos) {
+        //Serial.println(pos);
+        old_pos = pos;
+        ui_rotate(&ui, pos);
+    }
+    if (ui_get_screen(&ui) != ui_pos) {
+        Serial.print("Current screen ");
+        Serial.println(ui_get_screen(&ui));
+        ui_pos = ui_get_screen(&ui);
+    }   
+    button_evt_t evt = button_check(&enc_button);
+
+    switch (evt) {
+        case BUTTON_SHORT:
+            //Serial.println("SHORT");
+            ui_short_press(&ui);
+            break;
+
+        case BUTTON_LONG:
+            //Serial.println("LONG");
+            ui_long_press(&ui);
+            break;
+
+        default: 
+            break;
+    }
+    if (button_tick(&enc_button)) {
+        Serial.println("TICK");
+    }
+}
